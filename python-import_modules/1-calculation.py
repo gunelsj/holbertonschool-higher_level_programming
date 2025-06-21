dec62d6f0f0f44a7369b6949d28955912f926a42
gunelsj <gunelsj@code.edu.az>
2025-06-21 09:22:30 +0000

    Add calculator functions and import example

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from calculator_1 import add, sub, mul, div
 
 a = 10
@@ -10,4 +9,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
