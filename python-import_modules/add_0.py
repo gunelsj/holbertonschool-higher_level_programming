831863d7a34ccecac1d6a75b635c894e2ca32098
gunelsj <gunelsj@code.edu.az>
2025-06-21 12:35:04 +0000

    Add 0-import_add.py and add_0.py for simple import task

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+def add(a, b):
+    """My addition function
 
-a = 1
-b = 2
+    Args:
+        a: first integer
+        b: second integer
 
-if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    Returns:
+        The return value. a + b
+    """
+    return a + b
