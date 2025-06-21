fc02a1fe52843919f71248be174813457dea2e10
gunelsj <gunelsj@code.edu.az>
2025-06-21 10:05:47 +0000

    Fix import path for hidden_4 module from /tmp

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    sys.path.insert(0, "/tmp")
-
+    sys.path.append('/tmp')
     import hidden_4
 
-    for name in sorted(dir(hidden_4)):
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
