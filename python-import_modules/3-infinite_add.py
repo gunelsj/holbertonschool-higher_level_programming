d728d9587d3af92d622dc09334487e720320085d
gunelsj <gunelsj@code.edu.az>
2025-06-21 09:55:20 +0000

    Fix: print plain integer result of sum

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,6 @@
 import sys
 
 if __name__ == "__main__":
-    total = 0
-    for arg in sys.argv[1:]:
-        total += int(arg)
+    total = sum(int(arg) for arg in sys.argv[1:])
     print(total)
+
