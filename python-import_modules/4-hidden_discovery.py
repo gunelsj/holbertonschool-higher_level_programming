206b648fb9a30c2a57f9f9a1657677b52c3a1e74
gunelsj <gunelsj@code.edu.az>
2025-06-21 10:10:04 +0000

    Add solution for task 4 - hidden discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    sys.path.append('/tmp')
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in sorted(names):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
