9e2a3546d990d34f0e4d92242be91a5085f6ed9e
gunelsj <gunelsj@code.edu.az>
2025-06-21 10:22:44 +0000

    Task 4: Print names from compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import hidden_4
-    for name in sorted(dir(hidden_4)):
+    import importlib.util
+
+    spec = importlib.util.spec_from_file_location("hidden_4", "/tmp/hidden_4.pyc")
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
+
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
