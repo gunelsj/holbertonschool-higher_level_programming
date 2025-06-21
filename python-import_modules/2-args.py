30adfda0c793cc8e51b9f1cf1c267b83f250961e
gunelsj <gunelsj@code.edu.az>
2025-06-21 09:34:01 +0000

    Add 2-args.py script to print arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,8 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    # sys.argv[0] skriptin adıdır, ona görə 1-dən başlamaq lazımdır
+    # sys.argv[0] skriptin adıdır,
+    # ona görə 1-dən başlamaq lazımdır
     count = len(args)
 
     if count == 0:
@@ -15,4 +16,3 @@ if __name__ == "__main__":
 
     for i, arg in enumerate(args, start=1):
         print("{}: {}".format(i, arg))
-
