c70071e7103b94389499eb2af64e2f854cf57624
gunelsj <gunelsj@code.edu.az>
2025-06-27 07:29:48 +0000

    Fix pycodestyle: add 2 blank lines before class Rectangle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class Rectangle."""
 
+
 class Rectangle:
     """Class that defines a rectangle."""
 
@@ -11,13 +12,16 @@ class Rectangle:
         self.height = height
         Rectangle.number_of_instances += 1
 
+
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+
     @property
     def width(self):
         return self.__width
+
 
     @width.setter
     def width(self, value):
@@ -27,9 +31,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+
     @property
     def height(self):
         return self.__height
+
 
     @height.setter
     def height(self, value):
@@ -39,18 +45,22 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+
     def area(self):
         return self.__width * self.__height
+
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
+
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
